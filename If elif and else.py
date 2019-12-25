0f5dc1ee6c2e5a4acb01111ffb0e1945c1f43cfb
uzairiqbal91 <uzairiqbal91@gmail.com>
2019-12-25 19:57:40 +0500

    if elif else

---
+++ If elif and else.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# if some_condition :
+#     #execute some code
+# elif some_condition :
+#     #execute some code
+# else :
+#     #execute some code  
+
+
+# In[7]:
+
+
+hungry = True
+
+if hungry :
+    print('get me food')
+else:
+    print('I am not hungry')
+
+
+# In[9]:
+
+
+hungry = False
+
+if hungry :
+    print('get me food')
+else:
+    print('I am not hungry')
+
+
+# In[11]:
+
+
+
+amount = 5000
+
+if amount < 5000:
+    print('sorry your balance is much not enoguh for this transaction')
+
+elif amount == 5000:
+    print('transaction succesffull but your account will be 0 after transcation')
+
+# this condition is when amount is > 5000
+else : 
+    print('transaction succesffull')
+
+
+# In[12]:
+
+
+amount = 6000
+
+if amount < 5000:
+    print('sorry your balance is much not enoguh for this transaction')
+
+elif amount == 5000:
+    print('transaction succesffull but your account will be 0 after transcation')
+
+# this condition is when amount is > 5000
+else : 
+    print('transaction succesffull')
+
+
+# In[13]:
+
+
+amount = 3000
+
+if amount < 5000:
+    print('sorry your balance is much not enoguh for this transaction')
+
+elif amount == 5000:
+    print('transaction succesffull but your account will be 0 after transcation')
+
+# this condition is when amount is > 5000
+else : 
+    print('transaction succesffull')
+
+
+# In[ ]:
+
+
+
+
