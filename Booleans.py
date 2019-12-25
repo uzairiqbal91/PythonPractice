05faf9c7bbb2b5dfd858136ec405111615a25e68
uzairiqbal91 <uzairiqbal91@gmail.com>
2019-12-25 18:47:58 +0500

    boolean

---
+++ Booleans.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+True
+
+
+# In[2]:
+
+
+False
+
+
+# In[3]:
+
+
+type(True)
+
+
+# In[4]:
+
+
+# we use Boolean in comparision operators
+1>3
+
+
+# In[5]:
+
+
+1 == 1
+
+
+# In[7]:
+
+
+b = 'none'
+
+
+# In[8]:
+
+
+b = 1>3
+
+
+# In[9]:
+
+
+b
+
+
+# In[10]:
+
+
+type(b)
+
+
+# In[ ]:
+
+
+
+
