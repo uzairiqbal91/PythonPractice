5d675c6bb33d7d1b8c407a3b5b1a70603aadb7e5
uzairiqbal91 <uzairiqbal91@gmail.com>
2019-12-25 19:47:18 +0500

    comparision with logical operators

---
+++ Chaining Comparison Operator.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#  ' and ' , ' or ', ' not ' are logical operaotrs 
+
+
+# In[7]:
+
+
+2 > 3 or 2 > 3
+
+
+# In[11]:
+
+
+1 == 1 and 1 == 2
+
+
+# In[12]:
+
+
+1 == 1 or 1 == 2
+
+
+# In[32]:
+
+
+
+
+
+# In[28]:
+
+
+not 1 != 1
+
+
+# In[35]:
+
+
+not 4000 < 3000
+
+
+# In[36]:
+
+
+not 4000 > 3000
+
+
+# In[ ]:
+
+
+
+
