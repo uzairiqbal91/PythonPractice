f2d593696b6e18849de228a900769bac7283ce72
uzairiqbal91 <uzairiqbal91@gmail.com>
2019-12-31 02:55:03 +0500

    Errors and exception handling

---
+++ Errors and exception handling.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# we can use error handling to let the script continue with other code , even if there is an error
+# try:
+#     this block of code to be attempted (may lead an error)
+
+# except:
+#     will execute in case there is an error in try block
+
+# finally:
+#     A final block of code to be executed , regardless of an error
+    
+
+
+# In[2]:
+
+
+def sum(num1,num2):
+    return num1+num2
+
+
+# In[3]:
+
+
+sum(3,4)
+
+
+# In[6]:
+
+
+number1 = 10
+
+
+# In[7]:
+
+
+number2 = input("please enter number")
+
+
+# In[10]:
+
+
+# this will throw an error because input returns in string but we need intger type
+
+sum(number1,number2)
+
+# this will not execute because of an error in previous line
+
+print("hey let me execute")
+
+
+# In[23]:
+
+
+try:
+    result = 10+"10"
+
+except:
+    print("cant add because of unmatch datatype")
+
+else : 
+    print("I execute when their is no error")
+    
+finally:
+    print("I always execute")
+    
+
+# this will exceute ecen their is an error in previous line
+# or put in finally block 
+print("hey let me execute")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
