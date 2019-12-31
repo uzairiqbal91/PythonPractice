8685fb926aa114c089530b47331813e1314e7a2c
uzairiqbal91 <uzairiqbal91@gmail.com>
2019-12-31 17:01:26 +0500

    python debugger - pdb

---
+++ Python debugger - pdb.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pdb
+
+
+# In[ ]:
+
+
+x = [1,2,3,4]
+y=3
+z=3
+
+result = y+z
+
+print(result)
+
+pdb.set_trace()
+
+# in this line throw an error , so we can check values by using debugger - pdb
+result2 = y+x
+print(resul2)
+
+
+# In[ ]:
+
+
+
+
