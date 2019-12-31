97cef0ca109e8d5545a11bf07ff2cdcffaa77406
uzairiqbal91 <uzairiqbal91@gmail.com>
2019-12-31 18:48:51 +0500

    Advanced Numbers

---
+++ Advanced Numbers.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+hex(12)
+
+
+# In[2]:
+
+
+hex(512)
+
+
+# In[3]:
+
+
+bin(1234)
+
+
+# In[4]:
+
+
+2**4 # power but you can use pow function as well for it
+
+
+# In[7]:
+
+
+pow(2,4)
+
+
+# In[8]:
+
+
+# for absolute value
+abs(-123)
+
+
+# In[9]:
+
+
+# for round value
+round(3.1)
+
+
+# In[10]:
+
+
+round(3.9)
+
+
+# In[12]:
+
+
+# you can give the floating pount number aswell
+
+round(3.141592,2)
+
+
+# In[ ]:
+
+
+
+
